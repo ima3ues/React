dea4887ede5a992163fd5dfc500782c3ee31ba94
sue <spuahss@gmail.com>
2025-07-08 20:27:00 +0800

    first commit

---
+++ header-footer-food-copyright-hyperlink/src/Header.jsx
@@ -0,0 +1,33 @@
+function Header() {
+
+    return(
+        <header>
+            <h1>
+                My Website
+            </h1>
+
+            <nav>
+                <ul>
+                    <li><a herf = "#">
+                        Home
+                    </a></li>
+                    <li><a herf = "#">
+                        About
+                    </a></li>
+                    <li><a herf = "#">
+                        Services
+                    </a></li>
+                    <li><a herf = "#">
+                        Contact
+                    </a></li>
+                </ul>
+            </nav>
+
+            <hr></hr>
+
+        </header>
+    );
+
+}
+
+export default Header
